8ed7e841f2e68a03fa53a33faad0da4ceb17f6e2
philoyou11 <18951665167@sina.cn>
2015-12-03 12:41:17 +0800

    fix id
    fix id

--- imoocEjs/routes/index.js
+++ imoocEjs/routes/index.js
@@ -6,7 +6,7 @@ var express = require('express'),
     router = express.Router();
 
 mongoose.connect('mongodb://localhost/imooc');
-
+  
 /* GET home page. */
 router.get('/', function(req, res, next) {
   MovieModel.fetch(function(err, movies){
@@ -17,50 +17,6 @@ router.get('/', function(req, res, next) {
       title  : 'Index',
       des    : 'index page',
       movies : movies
-      /*
-      movies : [
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-        {
-          id      : 100,
-          picurl  : "/images/jewel.jpg",
-          title   : "终结者",
-        },
-      ],
-      */
     });
   });
 });
@@ -77,7 +33,7 @@ router.get('/admin/movie', function(req, res, next) {
 
 router.post('/admin/movie/new', function(req, res){
   var movieObj = req.body.movie,
-      id = movieObj.id,
+      id = movieObj._id,
       _movie;
   if(id !== 'undefined'){
     MovieModel.findById(id, function(err, movie){
@@ -150,17 +106,6 @@ router.get('/movie/:id', function(req, res, next) {
       title : 'detail' + movie.title,
       des   : 'detail page',
       movie : movie
-      /*
-      movie : {
-        flash    : 'http://www.w3school.com.cn/i/movie.ogg',
-        title    : '终结者',
-        director : 'James',
-        country  : 'USA',
-        language : 'English',
-        year     : 2001,
-        summary  : '在一次人类登陆火星的任务中，宇航员马克·沃特尼（马特·达蒙 Matt Damon 饰）经历了一场恶劣的风暴后，与他的机组成员失联，所有人都认为他在这次任务中丧生。然而，马克却幸运地活了下来，然而他发现自己孤单地置身于异星球。面对贫乏的生命补给，马克必须用他的聪明才智和顽强的精神存活下来，并找寻向地球发出“他还活着”求救信号的方法，而地球上的人也面临着是否前往火星去拯救他的分歧。 '
-      },
-      */
     });
   });
 });
